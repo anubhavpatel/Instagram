e2db7a30108b581add7fc6bbd11fcf0a7c0764b4
Anubhav Patel <anubhavpatel6391@gmail.com>
2021-08-18 00:06:08 +0530

    Added Firebase

--- app/src/main/java/com/example/instagram/SignInActivity.kt
+++ app/src/main/java/com/example/instagram/SignInActivity.kt
@@ -1,35 +1,132 @@
 package com.example.instagram
 
+import android.app.ProgressDialog
 import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.text.TextUtils
+import android.util.Log
+import android.widget.*
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class SignInActivity : AppCompatActivity() {
+    companion object{
+        private const val RC_SIGN_IN =120
+    }
     private lateinit var signUp : TextView
+    private lateinit var googleSignIn:ImageView
     private lateinit var btnLogin : Button
     private lateinit var emailSignIn: EditText
+    private lateinit var passwordSignIn: EditText
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
         signUp=findViewById(R.id.signUp)
+        emailSignIn=findViewById(R.id.emailSignIn)
+        passwordSignIn=findViewById(R.id.passwordSignIn)
+        googleSignIn=findViewById(R.id.googleSignIn)
         btnLogin=findViewById(R.id.btnLogin)
-         auth = Firebase.auth
+         auth = FirebaseAuth.getInstance()
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
         signUp.setOnClickListener {
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
 
         }
         btnLogin.setOnClickListener {
-            val i = Intent(this,MainActivity::class.java)
-            startActivity(i)
+            loginUser()
+        }
+        googleSignIn.setOnClickListener {
+            signIn()
+        }
+    }
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if(task.isSuccessful){
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("SignInActivity", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("SignInActivity", "Google sign in failed", e)
+                }
+            }else{
+                Log.w("SignInActivity", exception.toString())
+            }
+          
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("SignInActivity", "signInWithCredential:success")
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("SignInActivity", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    private fun loginUser() {
+        val  email = emailSignIn.text.toString()
+        val  password = passwordSignIn.text.toString()
+        when{
+            TextUtils.isEmpty(email) -> Toast.makeText(this,"email is required", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this,"password name is required", Toast.LENGTH_LONG).show()
+            else -> {
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Sign Up")
+                progressDialog.setMessage("Please wait,this may take a while")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(baseContext, " Login successfully",Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this,MainActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                            finish()
+                            progressDialog.dismiss()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(baseContext, "incorrect data",Toast.LENGTH_SHORT).show()
+                            progressDialog.dismiss()
+                        }
+                    }
+            }
         }
     }
 
@@ -46,5 +143,4 @@ class SignInActivity : AppCompatActivity() {
             finish()
         }
     }
-
 }
--- app/src/main/java/com/example/instagram/SplashScreenActivity.kt
+++ app/src/main/java/com/example/instagram/SplashScreenActivity.kt
@@ -6,12 +6,10 @@ import android.os.Bundle
 import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
-    lateinit var handler : Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        handler = Handler()
-        handler.postDelayed({
+        Handler().postDelayed({
             val intent = Intent(this,SignInActivity::class.java)
             startActivity(intent)
             finish()
